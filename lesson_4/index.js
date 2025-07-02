1ca039ccdf293c5fc288c9b5369aa573d51f16c7
Osman Zakaria <111977656+osmanzakaria532@users.noreply.github.com>
2025-07-02 14:12:10 +0600

    lesson_4

---
+++ lesson_4/index.js
@@ -0,0 +1,84 @@
+const eggPrice = 10;
+const milkPrice = 20;
+const breadPrice = 15;
+
+const totalPrice = eggPrice + milkPrice + breadPrice;
+// console.log(`Total price of groceries: ${totalPrice} units`);
+
+// * স্টার / এসে্টেরিস্ক চিহ্ন
+
+// subtraction, multiplication, division
+
+const Name = "Osman";
+const dairy = "Dairy";
+
+const message = `My name is ${Name} and I love to write ${dairy}.`;
+// console.log(message);
+
+// Task 1:
+const income = 100;
+const expenses = 40;
+const savings = income - expenses;
+// console.log(`My savings are: ${savings} units`);
+
+// Task 2:
+const pencils = 10;
+const usePencils = 2;
+const everyTimesCanUse = pencils / usePencils;
+// console.log(`I can use the pencils ${everyTimesCanUse} times`);
+
+// Task 3:
+
+const totalTaka = 60;
+const costPerTime = 15;
+const timesCanSpend = totalTaka / costPerTime;
+const takaLeft = totalTaka % costPerTime;
+// console.log(
+//   `I can use the taka ${everyTimesCanUseTaka} times and I have ${takaLeft} taka left`
+// );
+
+// Task 4:
+const hello = "Hello";
+const world = "World";
+const helloWorld = `${hello} ${world}`;
+// console.log(`The message is: ${helloWorld}`);
+
+// Task 5:
+const taka = 500;
+const everyTimesCanUseTaka2 = taka / 75;
+const takaLeft2 = taka % 75;
+// console.log(
+//   `I can use the taka ${everyTimesCanUseTaka2} times and I have ${takaLeft2} taka left`
+// );
+
+// Task 6:
+
+// console.log(8 / 3);
+// console.log(8 % 3);
+
+// Task 7:
+const num1 = 50;
+const num2 = 9;
+const bagSes = num1 % num2;
+// console.log(`The bag ses is: ${bagSes}`);
+
+// Task 8:
+
+const bangla = "Bangla";
+const desh = "Desh";
+const banglaDesh = bangla + desh;
+console.log(`This: ${banglaDesh}`);
+
+// Task 9:
+const totalRiceMon = 12;
+const monToKegi = 40;
+const totalRiceKg = totalRiceMon * monToKegi;
+
+const dailyRice = 4;
+const totalDays = totalRiceKg / dailyRice;
+
+const daysInMonth = 30;
+const totalMonths = totalDays / daysInMonth;
+
+console.log(totalDays);
+console.log(totalMonths);
